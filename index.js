778353d563baea844fd3ba0cf78135fc2a1c635f
Yusuf Dumlu <yusuf.ex@gmail.com>
2023-02-06 03:36:43 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
-}
+
+const input = document.getElementById('button')
+    input.addEventListener('click' , () => {
+        alert('I was Clicked');
+    });
+};
+
+addingEventListener()
